654058bb79cab0038da4bc06168c009bf082d0c3
JADPA-22 <JADPA-22@DESKTOP-1KKEJ7M>
2023-04-22 10:02:22 -0600

    Agregar archivos de proyecto.

---
+++ ExamenFanorPaz/Models/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExamenFanorPaz.Models
+{
+    public partial class Producto
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string? Nombre { get; set; }
+        public string? Descripcion { get; set; }
+        public string? Categoria { get; set; }
+    }
+}
---
+++ ExamenFanorPaz/Models/db_a9815a_fanortestContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ExamenFanorPaz.Models
+{
+    public partial class db_a9815a_fanortestContext : DbContext
+    {
+        public db_a9815a_fanortestContext()
+        {
+        }
+
+        public db_a9815a_fanortestContext(DbContextOptions<db_a9815a_fanortestContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Producto> Productos { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Server=sql8005.site4now.net;Database=db_a9815a_fanortest;User Id=db_a9815a_fanortest_admin;password=Admin2023!;Trusted_Connection=False;MultipleActiveResultSets=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Producto>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Categoria)
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Descripcion)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("ID");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
